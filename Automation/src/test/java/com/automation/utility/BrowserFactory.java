220d085a83f8f9ac96367d59291b87cacdc52f0b
Pallavi <pallavi.hd@reconnectenergy.com>
2021-06-05 15:10:16 +0530

    added chrome driver, browser factory .

---
+++ Automation/src/test/java/com/automation/utility/BrowserFactory.java
@@ -0,0 +1,45 @@
+package com.automation.utility;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * @Pallavi
+ * This class is to start browser, enter URL & close the browser.
+ */
+public class BrowserFactory 
+{
+	public static WebDriver startApplication(WebDriver driver,String browserName,String appURL) 
+	{
+		if(browserName.equals("Chrome")) 
+		{
+			System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+			driver=new ChromeDriver();
+		}
+		else if(browserName.equals("Firefox")) 
+		{
+			
+		}
+		else if(browserName.equals("IE")) 
+		{
+			
+		}
+		else 
+		{
+			System.out.println("we do not support this browser");
+		}
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(appURL);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		return driver;
+	}
+	
+	public static void quitBrowser(WebDriver driver) 
+	{
+		driver.quit();
+	}
+
+}
